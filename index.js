dd30b4380d79d93b7a724573f609af53f1b03e4a
partycrusher546 <muthonidennis546@gmail.com>
2024-03-20 22:45:59 +0300

    complete task

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+if (multiply === 62) {
+  console.log(`The product of ${num1} and ${num2} is 62.`);
+} else {
+  console.log(`Oops! The product is not 62. Try different values for num1 and num2.`);
+}
+
+const random = Math.floor(Math.random() * 100) + 1;
+console.log(`Random number: ${random}`);
+
+const num3 = 25;
+const num4 = 7;
+const mod = (num3 % num4);
+
+if (mod === 4) {
+  console.log(`The remainder of ${num3} divided by ${num4} is 4.`);
+} else {
+  console.log(`The remainder does not meet the requirement.`);
+}
+
+
+
+const numbers = [10, 5, 20, 15, 8];
+
+
+const max = Math.max(...numbers);
+
+console.log("The highest number is:", max);
