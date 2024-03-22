25f1cf7a1705865b1f974a769b1561b3cfb514cd
partycrusher546 <muthonidennis546@gmail.com>
2024-03-22 14:07:16 +0300

    compete task

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Write your code here
+// question 1
 const num1 = 31;
 const num2 = 2;
 const multiply = num1 * num2;
@@ -9,10 +10,10 @@ if (multiply === 62) {
     `Oops! The product is not 62. Try different values for num1 and num2.`
   );
 }
-
+//question 2
 const random = Math.floor(Math.random() * 100) + 1;
 console.log(`Random number: ${random}`);
-
+//question 3
 const num3 = 25;
 const num4 = 7;
 const mod = num3 % num4;
@@ -22,7 +23,7 @@ if (mod === 4) {
 } else {
   console.log(`The remainder does not meet the requirement.`);
 }
-
+//question 4
 const numbers = [10, 5, 20, 15, 8];
 
 const max = Math.max(...numbers);
